1e576e4763eafab145a2117792e1ba220aae8d94
Richard Lindner <richard.lindner@fluig.com>
2019-11-01 14:42:03 -0300

    Created router and components structure

---
+++ src/app/feature/child-a/child-a.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChildAComponent } from './child-a.component';
+
+
+
+@NgModule({
+  declarations: [ChildAComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChildAModule { }
